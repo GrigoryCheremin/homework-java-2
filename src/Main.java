49c4c02175cd89c84b2438f008b14cc5502f895a
GrigoryCheremin <Greenx.git.work@gmail.com>
2022-02-03 21:04:23 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 4990; // цена билета, руб.
+        int milePrice = 20; // цена бонусной мили, руб.
+        int milesQuantity = ticketPrice/milePrice;
+
+        System.out.println("Количество бонусных миль:");
+        System.out.println((milesQuantity) + " шт");
+    }
+}
