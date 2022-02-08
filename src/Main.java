8b1f7d967c33ce4942ab3635d499727184263513
GrigoryCheremin <Greenx.git.work@gmail.com>
2022-02-08 19:22:46 +0300

    reformate code 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int ticketPrice = 4990; // цена билета, руб.
+        int ticketPrice = 4999; // цена билета, руб.
         int milePrice = 20; // цена бонусной мили, руб.
-        int milesQuantity = ticketPrice/milePrice;
-
+        int milesQuantity = ticketPrice / milePrice;
         System.out.println("Количество бонусных миль:");
         System.out.println((milesQuantity) + " шт");
     }
